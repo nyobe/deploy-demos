f2062c2a6c7fbf5e05f4b227f36a24bfd420a25d
Claire Gaestel <213631+nyobe@users.noreply.github.com>
2024-10-08 12:56:14 -0700

    log ec2 instance-id

--- pulumi-programs/simple-resource/index.ts
+++ pulumi-programs/simple-resource/index.ts
@@ -1,5 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 
+pulumi.log.info("Who am I...?")
+let response = await fetch("http://169.254.169.254/latest/meta-data/instance-id");
+let instanceID = response.text()
+
 for(var i = 0; i < 100; i++){
-    pulumi.log.info("I am deploying...");
+    pulumi.log.info(`I am deploying on ${instanceID}...`);
 }
+
